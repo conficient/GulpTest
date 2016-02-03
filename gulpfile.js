2348045abea9f86333e5dd26391cca5fa84dd9c1
Howard Richards <dev@conficient.com>
2016-02-03 22:00:43 +0000

    TS output now merging

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var merge =require("merge2");
+var concat = require('gulp-concat');
 
 //
 //gulp.watch('scr/**/*.ts', function(event) {
@@ -7,15 +9,23 @@ var ts = require('gulp-typescript');
 //});
 //
 
-var tsOptions = {
+var tsProject = ts.createProject({
+	declaration: true,
+	noExternalResolve: true,
     module: "amd",
     noImplicitAny: true, 
-	out: 'output.js',
     removeComments: true
-};
+});
 
 gulp.task('default', function () {
-	return gulp.src('src/**/*.ts')
-		.pipe(ts(tsOptions))
-		.pipe(gulp.dest('built/local'));
+    var tsResult = gulp.src('src/**/*.ts')
+					.pipe(ts(tsProject));
+
+    return tsResult.js
+        .pipe(concat("Test.js"))                    
+		.pipe(gulp.dest('release/js'));
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch('src/**/*.ts', ['default']);
 });
