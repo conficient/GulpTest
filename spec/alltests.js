5471a39d07a731af399f3c3efb7db6617aaae1aa
Howard Richards <dev@conficient.com>
2016-02-05 18:02:30 +0000

    Amended test name

--- spec/alltests.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-describe("test 1", function () {
-    it("contains spec with an expectation", function () {
-        expect(true).toBe(true);
-    });
-    it("test failure", function () {
-        expect(true).toBe(false);
-    });
-});
